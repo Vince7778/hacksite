e060f502e778f40676156ac14301e03234ce0355
Vince7778 <realvince7778@gmail.com>
2021-09-15 18:35:17 -0700

    Remove ip address verification
    At school everyone has the same IP (probably) so IP address verification
    wouldn't work. Instead, localstorage is used, which is not perfect but
    it works.

--- index.js
+++ index.js
@@ -9,31 +9,11 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const app = express();
 const port = 80;
 
-let users = [];
 let winnerNames = {names:[]};
 let winnerCount = 0;
 
-function getUser(ip) {
-    return users.find(v => v.ip === ip);
-}
-
-function getAttempts(ip) {
-    let fnd = getUser(ip);
-    if (!fnd) {
-        users.push({
-            "ip": ip,
-            "attempts": 10,
-            "won": false
-        });
-        return 10;
-    }
-    return fnd.attempts;
-}
-
-
-
 app.get("/", (req, res) => {
-    console.log("sending")
+    console.log("sending", req.ip);
     res.sendFile(path.join(__dirname, "/static/index.html"));
 });
 
@@ -50,52 +30,23 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", urlencodedParser, (req, res) => {
-    if (getAttempts(req.ip) <= 0) {
-        res.redirect("/login?attemptOut=1");
-        return;
-    }
-
     let username = req.body.username;
     let password = req.body.password;
     console.log("attempt", req.ip, username, password);
 
     if (username === passwords.user1 && password === passwords.pass1) {
         console.log("password success");
-        const ip = req.ip;
-        const user = getUser(ip);
-        if (!user) {
-            users.push({
-                "ip": ip,
-                "attempts": 9,
-                "won": true
-            });
-            winnerCount++;
-        } else {
-            if (!user.won) winnerCount++;
-            user.won = true;
-            user.attempts--;
-        }
-        res.redirect("/winner");
+        winnerCount++;
+        res.redirect("/winner?pass="+passwords.pass1);
     } else {
-        const ip = req.ip;
-        const user = getUser(ip);
-        if (!user) {
-            users.push({
-                "ip": ip,
-                "attempts": 9,
-                "won": false
-            });
-        } else {
-            user.attempts--;
-        }
         console.log("password failure");
         res.redirect("/login?failed=1");
     }
 });
 
 app.get("/winner", (req, res) => {
-    const user = getUser(req.ip);
-    if (!user || !user.won) {
+    const pass = req.query.pass;
+    if (!pass || pass !== passwords.pass1) {
         res.send("You do not have permission to access this page.");
         return;
     }
@@ -105,27 +56,18 @@ app.get("/winner", (req, res) => {
 
 app.post("/winner", urlencodedParser, (req, res) => {
     let sname = req.body.sname;
-    let inArray = winnerNames.names.find(v => v.ip === req.ip);
 
-    if (!inArray) {
-        console.log("submitname", req.ip, sname);
-        winnerNames.names.push({
-            ip: req.ip,
-            name: sname
-        });
-        fs.writeFile("winners.json", JSON.stringify(winnerNames), () => {});
-        res.redirect("/winner?success=1");
-    } else {
-        res.redirect("/winner?failed=1");
-    }
+    console.log("submitname", req.ip, sname);
+    winnerNames.names.push({
+        ip: req.ip,
+        name: sname
+    });
+    fs.writeFile("winners.json", JSON.stringify(winnerNames), () => {});
+    res.redirect("/winner?success=1&pass="+passwords.pass1);
 });
 
 app.get("/api/winnerCount", (req, res) => {
     res.send(winnerCount.toString());
-});
-
-app.get("/api/attemptCount", (req, res) => {
-    res.send(getAttempts(req.ip).toString());
 });
 
 app.listen(port, () => {

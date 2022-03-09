51bed61dc673ac5afc8ae6780245e46c13a37057
Vince7778 <realvince7778@gmail.com>
2022-03-08 21:49:12 -0800

    Reformatted values

--- sqlchallenge/index.js
+++ sqlchallenge/index.js
@@ -14,7 +14,20 @@ function setupDB() {
     db.run("CREATE TABLE Users (name TEXT, money TEXT)");
     db.run("CREATE TABLE Users_New (name TEXT, nickname TEXT, cash_in_dollars INTEGER)");
 
-    db.run("INSERT INTO Users (name, money) VALUES ('Conor', 100), ('Mr. Osborne', 400), ('Aadit', -100), ('Thank you hacker!', 402), ('But our flag', 403), ('is in another table!', 404), ('(called \"Users_New\")', 405)");
+    const values = [
+        "('conor', 100)",
+        "('mr. osborne', 400)",
+        "('aadit', -100)",
+        "('elon musk', 0.01)",
+        "('admin', 123)",
+        "('me', 0)",
+        "('Thank you hacker!', 402)",
+        "('But our flag', 403)",
+        "('is in another table!', 404)",
+        "('(called \"Users_New\")', 405)"
+    ];
+
+    db.run("INSERT INTO Users (name, money) VALUES "+values.join(","));
     const randomValue = Math.floor(Math.random()*1000000);
     db.run(`INSERT INTO Users_New (name, nickname, cash_in_dollars) VALUES ('flag_${randomValue}', 'The answer is: ', ${flag})`);
 }
@@ -23,10 +36,10 @@ db.serialize(setupDB);
 router.use("/", express.static(path.join(__dirname, "static/index.html")));
 
 router.get("/queryName", (req, res) => {
-    const qry = req.query.name;
+    const qry = req.query.name.toLowerCase();
     if (!qry) {
         res.statusCode = 400;
-        res.json({status:400,err:'No name queried'});
+        res.json({status:400,err:'Missing name'});
         return;
     }
     
@@ -46,6 +59,11 @@ router.get("/queryName", (req, res) => {
             return;
         }
         console.log(rows);
+        if (rows.length === 0) {
+            res.statusCode = 400;
+            res.json({status:400,err:"No users found with username "+qry});
+            return;
+        }
         res.json({status:200,res:rows});
     });
 });
